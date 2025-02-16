92c8a1a1aa37c00dce4863ee5ca892576be1cf27
sujit patel <sujitworkclg@gmail.com>
2025-02-16 11:52:54 +0530

    Project Deploy Connection Add

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,8 +7,10 @@ export default defineConfig({
     port: 5001,
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: "https://real-time-chat-o5jx.onrender.com",
+        // target: "http://localhost:4000",
         changeOrigin: true,
+        secure: true,
       },
     }
   },
