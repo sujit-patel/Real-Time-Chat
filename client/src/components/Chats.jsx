711c4865d103faabd62957c6fa7055b4e0407b94
sujit patel <sujitworkclg@gmail.com>
2025-03-02 17:30:40 +0530

    done chat parts

--- client/src/components/Chats.jsx
+++ client/src/components/Chats.jsx
@@ -1,14 +1,40 @@
-import React, { useEffect } from "react";
-import useConversation from "../stateManage/useConversation.js";
+import React, { useEffect, useState } from "react";
 import Chat from "./Chat";
+import useGetMessage from "../context/useGetMessage.js";
+import Loading from "./Loading.jsx";
+import useConversation from "../stateManage/useConversation.js";
 
-const Chats = () => {
-  const { messages } = useConversation();
+function Chats() {
+  const { loading, messages } = useGetMessage();
+  const { selectedConversation } = useConversation();
+  const [showChat, setShowChat] = useState(false);
+
+  useEffect(() => {
+    setShowChat(false);
+    setTimeout(() => {
+      setShowChat(true);
+    }, 1000);
+    setShowChat(false);
+  }, [selectedConversation]);
+
   return (
-    <div className="chat-container h-full p-4 scrollbar-hide overflow-y-auto ">
-      {messages && messages.length > 0 ? (
-        messages.map((msg, index) => <Chat key={index} message={msg} />)
+    <div
+      className={`chat-container h-full ${
+        loading || !showChat ? "p-1" : "p-4"
+      } scrollbar-hide overflow-y-auto`}
+    >
+      {loading || !showChat ? (
+        <Loading />
       ) : (
+        messages.length > 0 &&
+        messages.map((message) => (
+          <div key={message._id}>
+            <Chat message={message} />
+          </div>
+        ))
+      )}
+
+      {!loading && messages.length === 0 && (
         <div className=" h-full flex items-center justify-center">
           <p className="text-3xl font-bold text-gray-500">
             Say! Hi 👋 to start the conversation
@@ -17,6 +43,6 @@ const Chats = () => {
       )}
     </div>
   );
-};
+}
 
 export default Chats;
