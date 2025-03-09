ad049c6c7b8488b773cc0adb374f6b7940193bec
sujit patel <sujitworkclg@gmail.com>
2025-03-09 12:27:31 +0530

    Update Server file

--- server/SocketIo/server.js
+++ server/SocketIo/server.js
@@ -17,18 +17,15 @@ const users = {};
 export const getReceiverSocketId = (receiverId) => users[receiverId];
 
 io.on("connection", (socket) => {
-    // console.log("New Client Connected:", socket.id);
 
     const userId = socket.handshake.query.userId;
 
     if (userId) {
         users[userId] = socket.id;
-        // console.log("User Connected:", users);
     }
 
     io.emit("onlineUsers", Object.keys(users));
     socket.on("disconnect", () => {
-        // console.log("Client Disconnected:", socket.id);
         for (const [key, value] of Object.entries(users)) {
             if (value === socket.id) {
                 delete users[key];
@@ -40,5 +37,3 @@ io.on("connection", (socket) => {
 });
 
 export { app, io, server };
-
-// git push origin HEAD --force
